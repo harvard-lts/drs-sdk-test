77577ee9d9950a3e0c6c65f9eba601581d3cd305
scossu <stefano@cossu.cc>
2024-01-30 14:08:35 -0500

    Initial commit: some functions.

---
+++ functions/aspace_integration_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from libnova.common.api import File
+from libnova.common.nuclio.Request import Request
+from libnova.common.api.JobMessage import JobMessageType
+
+
+def handler(context, event):
+    context.logger.info(event.body.decode("utf-8"))
+    request_helper = Request(context, event)
+
+    request_helper.job_init()
+
+    fids = request_helper.JSONData['function_data']['files']['ids']
+    request_helper.log(
+            f"The following objects will be pushed to ASpace: {fids}",
+            JobMessageType.INFO)
+
+    for fid in fids:
+        File.set_metadata(
+                fid,
+                '{"metadata": [{"iecode": "relation", "value": '
+                f'"aspace:{fid}"' '}]}')
+
+    request_helper.job_end(True)
